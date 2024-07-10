3d6b1433e81901c26afe962d7618af45684c30ff
Andrew Williams <Andrew.Williams@IRIS.CO.UK>
2024-07-10 11:50:45 +0100

    reworded spec

--- CarRental.Bookings/BookingService.cs
+++ CarRental.Bookings/BookingService.cs
@@ -1,6 +1,7 @@
 ﻿namespace CarRental.Bookings
 {
     using System;
+    using System.Collections.Generic;
     using CarRental.Bookings.Entities;
 
     public class BookingService
@@ -12,16 +13,18 @@
             _repo = repo;
         }
 
-        /* SPEC
-         * Book a car from a [start] date for a given number of days [duration].
+        /* Part one:
+         *      Book a car from a [start] date for a given number of days [duration].
          * 
-         * A specific car can only be booked if it isn't already booked out. After each car booking, 
-         * an extra day is required for cleaning and can't be booked out on this day.
+         *      A specific car can only be booked if it isn't already booked out. After each car booking, 
+         *      an extra day is required for cleaning and can't be booked out on this day.
          * 
-         * The [TotalCost] of a car booking is the cars [DailyCost] multiplied by the number of days. The
-         * agreed discount is then applied onto final value.
+         *      If the booking can't be made, raise an exception highlighting the specific error.
          * 
-         * If the booking can't be made, raise an exception highlighting the specific error.
+         * Part two:
+         *      The [TotalCost] of a car booking is the cars [DailyCost] multiplied by the number of days. The
+         *      agreed discount is then applied onto final value.
+         *
          */
         public Booking MakeCarBooking(Car car, DateTime start, int duration, float discount, string name)
         {
@@ -29,26 +32,33 @@
             booking.CarId = car.Id;
             booking.StartDate = start;
             booking.ReturnDate = start.AddDays(duration);
-            booking.TotalCost = 0; //todo
+            booking.TotalCost = 0; //todo:: part two
             booking.Name = name;
 
-            // todo:: check for clashes
-            // var bookings = _repo.GetCarBookings();
-            //_repo.AddCarBooking(booking);
+            // bookings contains a list of all historic, current and future bookings for all cars.
+            List<Booking> bookings = _repo.GetCarBookings();
+
+            // todo:: part one: check for booking clashes
+
+            _repo.AddCarBooking(booking);
 
             return booking;
         }
 
-        /* SPEC
+        /*  Part three:
          * Book a van from a [start] date for a given number of days [duration].
          * 
          * A specific van can only be booked if it isn't already booked out. No valet is required for a van.
          * 
-         * The daily rate for a van after 5 days is subject to a further 25% discount.
+         * If the booking can't be made, raise an exception highlighting the specific error.
+         * 
+         * The daily rate for a van after 5 days is subject to a further 25% discount. For example, if you 
+         * rent a van for 7 days at £100 per day, the first 5 days will be charged at £100 per day and the 
+         * remaining 2 days will be charged at £75 per day. 
+         * 
          * The [TotalCost] of a van booking is the vans [DailyCost] multiplied by the number of days. The
          * agreed discount is then applied onto final value.
          * 
-         * If the booking can't be made, raise an exception highlighting the specific error.
          */
     }
 }
